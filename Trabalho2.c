389ab00ff1d5bcba14fd1869314ee4938c3afd2f
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2024-04-24 16:57:51 -0300

    Update Trabalho2.c

--- Trabalho2.c
+++ Trabalho2.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Ponto {
+typedef struct {
     float X;
     float Y;
-};
+} Ponto;
+
+float AreaTriangulo (Ponto p1, Ponto p2, Ponto p3);
 
 main(){
 
     FILE *arquivo;
-    float area;
     int vertices;
+    float AreaPoligono;
 
     arquivo = fopen("triangulo5.txt", "r");
 
@@ -20,15 +22,44 @@ main(){
         exit(1);
     }
 
-    fscanf(arquivo, "%d", &vertices);
+    fscanf(arquivo, "%d", &vertices); // guarda o nmr de vertices
 
-    struct Ponto Vertices[vertices];
+    Ponto Vertices[vertices]; //cria um vetor de vertices
     
     for(int i = 0; i < vertices; i++){
-    
     fscanf(arquivo, "%f %f\n", &Vertices[i].X, &Vertices[i].Y);
-    printf("%f %f\n", Vertices[i].X, Vertices[i].Y);
+    } // guarda os vertices
 
+    if(vertices > 2){ 
+        
+        for(int i = 0; i < vertices - 2; i++){ //cria os triangulos e calcula a area de cada um
+        float determinante;
+        Ponto p1 = Vertices[0];
+        Ponto p2 = Vertices[1 + i];
+        Ponto p3 = Vertices[2 + i];
+        determinante = AreaTriangulo(p1, p2, p3);
+
+        AreaPoligono += determinante;
+        }
+    } else {
+        printf("Numero de vertices incompativel");
+        exit(0);
     }
+
+    printf("\nA area do poligono e %.2f\n", AreaPoligono);
+
     fclose(arquivo);
+}
+
+float AreaTriangulo (Ponto p1, Ponto p2, Ponto p3){
+    float AreaPoligono;
+    AreaPoligono = (p1.X * p2.Y * 1) + (p1.Y * 1 * p3.X) + (1 * p2.X * p3.Y) - (p3.X * p2.Y * 1) - (p3.Y * 1 * p1.X) - (1 * p2.X * p1.Y);
+
+    if(AreaPoligono < 0){
+        AreaPoligono *= -1;
+    }
+
+    AreaPoligono = AreaPoligono / 2;
+    
+    return AreaPoligono;
 }
